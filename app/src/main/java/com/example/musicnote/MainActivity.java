ee86f202106a75eb27e48f39f7c3c31aa6571662
HopangLee <hgknim87j@naver.com>
2020-07-26 01:51:27 +0900

    add intro code

--- app/src/main/java/com/example/musicnote/MainActivity.java
+++ app/src/main/java/com/example/musicnote/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PointF;
@@ -37,8 +38,10 @@ import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
@@ -61,6 +64,7 @@ import com.naver.maps.map.widget.ScaleBarView;
 import com.naver.maps.map.widget.ZoomControlView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static android.hardware.SensorManager.AXIS_X;
@@ -106,7 +110,7 @@ public class MainActivity extends AppCompatActivity
     private ModelRenderable orangeNoteRenderable;
     private ModelRenderable redNoteRenderable;
 
-    Handler mHandler = new Handler(); // 딜레이 시간
+    private ModelRenderable[] albumRenderable = new ModelRenderable[3]; // album Object들
 
     // Device Orientation 관련
     private SensorManager mSensorManager;
@@ -132,6 +136,7 @@ public class MainActivity extends AppCompatActivity
     TextView pitchText, rollText;
     Context context;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -139,6 +144,10 @@ public class MainActivity extends AppCompatActivity
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //팝업창 관련
+        Intent intent = new Intent(getApplicationContext(),PopupActivity.class);
+        startActivityForResult(intent,1);
 
         // Devicd Orientation 관련
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
@@ -302,6 +311,36 @@ public class MainActivity extends AppCompatActivity
                             return null;
                         }
                 );
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.guitar02)
+                .build().thenAccept(renderable -> albumRenderable[0] = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load albumRenderable 1 model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.guitar02)
+                .build().thenAccept(renderable -> albumRenderable[1] = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load albumRenderable 2 model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.guitar02)
+                .build().thenAccept(renderable -> albumRenderable[2] = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load albumRenderable 3 model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
     }
 
     @Override
@@ -408,80 +447,15 @@ public class MainActivity extends AppCompatActivity
                 if (mAnchorNode[i].getAnchor().getTrackingState() != TrackingState.TRACKING
                         && arSceneView.getArFrame().getCamera().getTrackingState() == TrackingState.TRACKING) {
                     // Detach the old anchor
+                    //mAnchorNode[i].getAnchor().detach();
+                    List<Node> children = new ArrayList<>(mAnchorNode[i].getChildren());
+                    for(Node n : children){
+                        n.setParent(null);
+                    }
                     mAnchorNode[i].getAnchor().detach();
 
-                    // Create a new anchor and attach it to the anchorNode.
-                    // Create an ARCore Anchor at the position.
-                    float dLatitude = (float) (markers[i].getLatitude() - mCurrentLocation.getLatitude()) * 110900f;
-                    float dLongitude = (float) (markers[i].getLongitude() - mCurrentLocation.getLongitude()) * 88400f;
-                    float distance = (float) Math.sqrt((dLongitude * dLongitude) + (dLatitude * dLatitude));
-
-                    if(distance > 15){ // 15m보다 멀면 오브젝트 생성X
-                        continue;
-                    }
-
-                    float height = -0.5f;
-                    Vector3 objVec = new Vector3(dLongitude, dLatitude, height);
-
-                    Vector3 xUnitVec;
-                    Vector3 yUnitVec;
-                    Vector3 zUnitVec;
-
-                    zUnitVec = new Vector3((float)(Math.cos(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.cos(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(-Math.sin(mCurrentPitch)));
-                    zUnitVec = zUnitVec.normalized().negated();
-
-                    yUnitVec = new Vector3((float)(Math.sin(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.sin(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(Math.cos(mCurrentPitch))).normalized();
-
-                    float wx = zUnitVec.x;
-                    float wy = zUnitVec.y;
-                    float wz = zUnitVec.z;
-
-                    float yx = yUnitVec.x;
-                    float yy = yUnitVec.y;
-                    float yz = yUnitVec.z;
-
-                    float t = 1 - (float)Math.cos(mCurrentRoll);
-                    float s = (float)Math.sin(mCurrentRoll);
-                    float c = (float)Math.cos(mCurrentRoll);
-
-                    float[][] rotMat = {{wx*wx*t+c, wx*wy*t+wz*s, wx*wz*t-wy*s},
-                            {wy*wx*t-wz*s, wy*wy*t+c, wy*wz*t+wx*s},
-                            {wz*wx*t+wy*s, wz*wy*t-wx*s, wz*wz*t+c}};
-
-                    yUnitVec = new Vector3(yx*rotMat[0][0] + yy*rotMat[0][1] + yz*rotMat[0][2],
-                            yx*rotMat[1][0] + yy*rotMat[1][1] + yz*rotMat[1][2],
-                            yx*rotMat[2][0] + yy*rotMat[2][1] + yz*rotMat[2][2]).normalized();
-
-                    xUnitVec = Vector3.cross(yUnitVec, zUnitVec).normalized();
-
-                    float xPos = Vector3.dot(objVec, xUnitVec);
-                    float yPos = Vector3.dot(objVec, yUnitVec);
-                    float zPos = Vector3.dot(objVec, zUnitVec);
-
-                    Vector3 xAxis = arSceneView.getScene().getCamera().getRight().normalized().scaled(xPos);
-                    Vector3 yAxis = arSceneView.getScene().getCamera().getUp().normalized().scaled(yPos);
-                    Vector3 zAxis = arSceneView.getScene().getCamera().getBack().normalized().scaled(zPos);
-                    Vector3 objectPos = new Vector3(xAxis.x + yAxis.x + zAxis.x, xAxis.y + yAxis.y + zAxis.y, xAxis.z + yAxis.z + zAxis.z);
-                    Vector3 cameraPos = arSceneView.getScene().getCamera().getWorldPosition();
-
-                    Vector3 position = Vector3.add(cameraPos, objectPos);
-
-                    // Create an ARCore Anchor at the position.
-                    Pose pose = Pose.makeTranslation(position.x, position.y, position.z);
-                    Anchor anchor = arSceneView.getSession().createAnchor(pose);
-
-                    mAnchorNode[i].setAnchor(anchor);
-                    mAnchorNode[i].setParent(arSceneView.getScene());
-
-                    Node node = new Node();
-
-                    node.setRenderable(foxRenderable);
-                    //node.setLocalScale(new Vector3(0.75f, 0.75f, 0.75f));
-                    node.setParent(mAnchorNode[i]);
-
-                    music(node, i);
-
-                    Toast.makeText(context, i + "번째 오브젝트 재생성", Toast.LENGTH_SHORT).show();
+                    createNode(i);
+                    if(createNode(i) == false) continue;
                 }
             }
             return;
@@ -492,9 +466,16 @@ public class MainActivity extends AppCompatActivity
             return;
         }
 
-        if (andyRenderable == null || foxRenderable == null || bofLogoRenderable == null || redNoteRenderable == null || orangeNoteRenderable == null) {
+        if (bofLogoRenderable == null || redNoteRenderable == null || orangeNoteRenderable == null) {
             Log.d(TAG, "onUpdate: objectRenderable is null");
             return;
+        }
+
+        for(ModelRenderable m : albumRenderable){
+            if(m == null){
+                Log.d(TAG, "onUpdate: objectRenderable is null");
+                return;
+            }
         }
 
         if (arSceneView.getArFrame() == null) {
@@ -514,105 +495,109 @@ public class MainActivity extends AppCompatActivity
             if (mAnchorNode[i] == null && mCurrentLocation != null) {
                 //Log.d(TAG, "onUpdate: mAnchorNode["+ i +"] is null");
                 // 여기에다가 내 gps의 위도 경도, 마커들의 위도 경도를 이용하여 마커들의 Pose값 구해야함!
-
-                float dLatitude = (float) (markers[i].getLatitude() - mCurrentLocation.getLatitude()) * 110900f;
-                float dLongitude = (float) (markers[i].getLongitude() - mCurrentLocation.getLongitude()) * 88400f;
-                float distance = (float) Math.sqrt((dLongitude * dLongitude) + (dLatitude * dLatitude));
-
-                /*
-                if( i == 0 ) {
-                    dLatitude = 2f;
-                    dLongitude = 0f;
-                }
-                else if ( i == 1 ){
-                   dLatitude = -2f;
-                   dLongitude = 0f;
-                }
-                else{
-                   dLatitude = 0f;
-                   dLongitude = 2f;
-                }*/
-
-
-                if(distance > 15){ // 15m보다 멀면 오브젝트 생성X
-                    continue;
-                }
-
-
-                //mCurrentAzim = 0f;
-                //mCurrentPitch = 0.785398f;
-                //mCurrentRoll = 0f;
-
-                float height = -0.5f;
-                Vector3 objVec = new Vector3(dLongitude, dLatitude, height);
-
-                Vector3 xUnitVec;
-                Vector3 yUnitVec;
-                Vector3 zUnitVec;
-
-                zUnitVec = new Vector3((float)(Math.cos(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.cos(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(-Math.sin(mCurrentPitch)));
-                zUnitVec = zUnitVec.normalized().negated();
-
-                yUnitVec = new Vector3((float)(Math.sin(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.sin(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(Math.cos(mCurrentPitch))).normalized();
-
-                float wx = zUnitVec.x;
-                float wy = zUnitVec.y;
-                float wz = zUnitVec.z;
-
-                float yx = yUnitVec.x;
-                float yy = yUnitVec.y;
-                float yz = yUnitVec.z;
-
-                float t = 1 - (float)Math.cos(mCurrentRoll);
-                float s = (float)Math.sin(mCurrentRoll);
-                float c = (float)Math.cos(mCurrentRoll);
-
-                float[][] rotMat = {{wx*wx*t+c, wx*wy*t+wz*s, wx*wz*t-wy*s},
-                                    {wy*wx*t-wz*s, wy*wy*t+c, wy*wz*t+wx*s},
-                                    {wz*wx*t+wy*s, wz*wy*t-wx*s, wz*wz*t+c}};
-
-                yUnitVec = new Vector3(yx*rotMat[0][0] + yy*rotMat[0][1] + yz*rotMat[0][2],
-                                    yx*rotMat[1][0] + yy*rotMat[1][1] + yz*rotMat[1][2],
-                                    yx*rotMat[2][0] + yy*rotMat[2][1] + yz*rotMat[2][2]).normalized();
-
-
-                xUnitVec = Vector3.cross(yUnitVec, zUnitVec).normalized();
-
-                /*
-                Log.d(TAG, "디버그 xVector <"+xUnitVec.x +", "+xUnitVec.y+", "+ xUnitVec.z +">");
-                Log.d(TAG, "디버그 yVector <"+yUnitVec.x +", "+yUnitVec.y+", "+ yUnitVec.z +">");
-                Log.d(TAG, "디버그 zVector <"+zUnitVec.x +", "+zUnitVec.y+", "+ zUnitVec.z +">");*/
-
-                float xPos = Vector3.dot(objVec, xUnitVec);
-                float yPos = Vector3.dot(objVec, yUnitVec);
-                float zPos = Vector3.dot(objVec, zUnitVec);
-
-                Vector3 xAxis = arSceneView.getScene().getCamera().getRight().normalized().scaled(xPos);
-                Vector3 yAxis = arSceneView.getScene().getCamera().getUp().normalized().scaled(yPos);
-                Vector3 zAxis = arSceneView.getScene().getCamera().getBack().normalized().scaled(zPos);
-                Vector3 objectPos = new Vector3(xAxis.x + yAxis.x + zAxis.x, xAxis.y + yAxis.y + zAxis.y, xAxis.z + yAxis.z + zAxis.z);
-                Vector3 cameraPos = arSceneView.getScene().getCamera().getWorldPosition();
-
-                Vector3 position = Vector3.add(cameraPos, objectPos);
-
-                // Create an ARCore Anchor at the position.
-                Pose pose = Pose.makeTranslation(position.x, position.y, position.z);
-                Anchor anchor = arSceneView.getSession().createAnchor(pose);
-
-                mAnchorNode[i] = new AnchorNode(anchor);
-                mAnchorNode[i].setParent(arSceneView.getScene());
-
-                Node node = new Node();
-
-                node.setRenderable(foxRenderable);
-                //node.setLocalScale(new Vector3(0.75f, 0.75f, 0.75f));
-                node.setParent(mAnchorNode[i]);
-
-                music(node, i);
-
-                Toast.makeText(context, "오브젝트 생성[" + i + "] (distance: " + distance + "m)", Toast.LENGTH_SHORT).show();
+                if(createNode(i) == false) continue;
             }
         }
+    }
+
+    public boolean createNode(int i){
+        float dLatitude = (float) (markers[i].getLatitude() - mCurrentLocation.getLatitude()) * 110900f;
+        float dLongitude = (float) (markers[i].getLongitude() - mCurrentLocation.getLongitude()) * 88400f;
+
+        if( i == 0 ) {
+            dLatitude = 3f;
+            dLongitude = 0f;
+        }
+        else if ( i == 1 ){
+            dLatitude = -3f;
+            dLongitude = 0f;
+        }
+        else{
+            dLatitude = 0f;
+            dLongitude = 3f;
+        }
+
+        float distance = (float) Math.sqrt((dLongitude * dLongitude) + (dLatitude * dLatitude));
+
+        if(distance > 15){ // 15m보다 멀면 오브젝트 생성X
+            return false;
+        }
+        float height = -0.5f;
+        Vector3 objVec = new Vector3(dLongitude, dLatitude, height);
+
+        Vector3 xUnitVec;
+        Vector3 yUnitVec;
+        Vector3 zUnitVec;
+
+        zUnitVec = new Vector3((float)(Math.cos(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.cos(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(-Math.sin(mCurrentPitch)));
+        zUnitVec = zUnitVec.normalized().negated();
+
+        yUnitVec = new Vector3((float)(Math.sin(mCurrentPitch) * Math.sin(mCurrentAzim)), (float)(Math.sin(mCurrentPitch) * Math.cos(mCurrentAzim)), (float)(Math.cos(mCurrentPitch))).normalized();
+
+        float wx = zUnitVec.x;
+        float wy = zUnitVec.y;
+        float wz = zUnitVec.z;
+
+        float yx = yUnitVec.x;
+        float yy = yUnitVec.y;
+        float yz = yUnitVec.z;
+
+        float t = 1 - (float)Math.cos(mCurrentRoll);
+        float s = (float)Math.sin(mCurrentRoll);
+        float c = (float)Math.cos(mCurrentRoll);
+
+        float[][] rotMat = {{wx*wx*t+c, wx*wy*t+wz*s, wx*wz*t-wy*s},
+                {wy*wx*t-wz*s, wy*wy*t+c, wy*wz*t+wx*s},
+                {wz*wx*t+wy*s, wz*wy*t-wx*s, wz*wz*t+c}};
+
+        yUnitVec = new Vector3(yx*rotMat[0][0] + yy*rotMat[0][1] + yz*rotMat[0][2],
+                yx*rotMat[1][0] + yy*rotMat[1][1] + yz*rotMat[1][2],
+                yx*rotMat[2][0] + yy*rotMat[2][1] + yz*rotMat[2][2]).normalized();
+
+
+        xUnitVec = Vector3.cross(yUnitVec, zUnitVec).normalized();
+
+        float xPos = Vector3.dot(objVec, xUnitVec);
+        float yPos = Vector3.dot(objVec, yUnitVec);
+        float zPos = Vector3.dot(objVec, zUnitVec);
+
+        Vector3 xAxis = arSceneView.getScene().getCamera().getRight().normalized().scaled(xPos);
+        Vector3 yAxis = arSceneView.getScene().getCamera().getUp().normalized().scaled(yPos);
+        Vector3 zAxis = arSceneView.getScene().getCamera().getBack().normalized().scaled(zPos);
+        Vector3 objectPos = new Vector3(xAxis.x + yAxis.x + zAxis.x, xAxis.y + yAxis.y + zAxis.y, xAxis.z + yAxis.z + zAxis.z);
+        Vector3 cameraPos = arSceneView.getScene().getCamera().getWorldPosition();
+
+        Vector3 position = Vector3.add(cameraPos, objectPos);
+
+        // Create an ARCore Anchor at the position.
+        Pose pose = Pose.makeTranslation(position.x, position.y, position.z);
+        Anchor anchor = arSceneView.getSession().createAnchor(pose);
+
+        mAnchorNode[i] = new AnchorNode(anchor);
+        mAnchorNode[i].setParent(arSceneView.getScene());
+
+        Node node = new Node();
+
+        node.setRenderable(albumRenderable[i]);
+        /*
+        node.setRenderable(bofLogoRenderable);
+
+        Camera camera = arSceneView.getScene().getCamera();
+        Vector3 d = Vector3.add(Vector3.up(), camera.getLeft()).normalized();
+        //d = Vector3.add(d, Vector3.forward().scaled(2)).normalized();
+        Quaternion direction = Quaternion.lookRotation(d, Vector3.up());
+        node.setWorldRotation(direction);
+        Vector3 vec = Vector3.add(cameraPos, camera.getLeft().scaled(5));
+        node.setWorldPosition(vec);*/
+
+        node.setLocalScale(new Vector3(0.25f, 0.25f, 0.25f));
+        node.setParent(mAnchorNode[i]);
+
+        music(node, i);
+
+        Toast.makeText(context, "오브젝트 생성[" + i + "] (distance: " + distance + "m)", Toast.LENGTH_SHORT).show();
+
+        return true;
     }
 
     public void music(Node node,int i){
